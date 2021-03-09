fa79e9a132f15d0d34ca7984823f2603d444b5b1
sugar01 <sugartime@nate.com>
2021-03-09 13:12:03 +0900

    CalculatePeriods

--- src/org/point85/workschedule/Main.java
+++ src/org/point85/workschedule/Main.java
@@ -4,6 +4,7 @@ import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
@@ -19,6 +20,32 @@ public class Main {
 	// write your code here
         Main m = new Main();
         m.firstTest();
+    }
+
+    // 해당날짜 에 여러개의 팀이 근무 투입될때
+    // 한팀은 주간근무, 한팀은 주간근무, 한팀은 주간근무, 한팀은 야간근무 일때
+    // 지정된 로테이션 (주,주,주,야,비,야,,비) 중 어떤 근무의 시작에 해당하는지 설정하고
+    // preriods 를 변경하여 설정한다..
+    public  List<TimePeriod> calculatePeriods(Rotation rotation,int startIndex){
+        List<TimePeriod> periods = rotation.getPeriods();
+
+        int periodsSize = periods.size();
+        int loopLen = periods.size()-1;
+
+        List<TimePeriod>periodsTmp= new ArrayList<>();
+        int loopCnt=1; //루프를 몇번돌았는지확인
+        for(int i=startIndex;i<=loopLen;i++){
+            periodsTmp.add(periods.get(i));
+            loopCnt++;
+        }
+
+        //나머지
+        if(loopCnt<periodsSize){
+            for(int j=0;j<periodsSize-loopCnt;j++){
+                periodsTmp.add(periods.get(j));
+            }
+        }
+         return periodsTmp;
     }
 
     public void firstTest() throws Exception {
@@ -37,9 +64,11 @@ public class Main {
         // rotation (주주주야비야비 예제)
         Rotation rotation = new Rotation("DNO", "DNO");
         rotation.addSegment(day, 3, 0,0);  //주간근무 3일
-        rotation.addSegment(night, 1, 1,0); //야간근무 1일 다음날휴무
-        rotation.addSegment(night, 1, 1,0); //야간근무 1일 다음날휴무
+        rotation.addSegment(night, 1, 1,0); //야간근무 1일 다음날비번
+        rotation.addSegment(night, 1, 1,0); //야간근무 1일 다음날비번
 
+
+        calculatePeriods(rotation,1);
 
         // rotation (주주야비야비주 예제)
         Rotation rotation2 = new Rotation("DNO", "DNO");
